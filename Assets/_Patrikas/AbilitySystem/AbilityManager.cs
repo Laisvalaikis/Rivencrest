483cf0a174647929834ad6c0d5bf1a317eed9ea1
PatrikasA <45782374+PatrikasA@users.noreply.github.com>
2023-08-22 16:31:37 +0300

    Added ability manager

---
+++ Assets/_Patrikas/AbilitySystem/AbilityManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class AbilityManager : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private GameTileMap gameTileMap;
+    private Vector2 _mousePosition;
+
+    public void OnMouseClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            MouseClick();
+    }
+
+    public void SetCurrentAbility()
+    {
+        
+    }
+    private void MouseClick()
+    {
+        Vector3 mousePos = new Vector3(_mousePosition.x, _mousePosition.y, GetComponent<Camera>().nearClipPlane);
+        Vector3 worldPos = GetComponent<Camera>().ScreenToWorldPoint(mousePos);
+        Debug.Log(worldPos);
+        if (!gameTileMap.CharacterIsSelected())
+        {
+            gameTileMap.SelectTile(worldPos);
+        }
+        else
+        {
+            if (!gameTileMap.CharacterIsOnTile(worldPos))
+            {
+                gameTileMap.MoveSelectedCharacter(worldPos, new Vector3(0, 0.5f,0));
+            }
+            else if(gameTileMap.CharacterIsSelected() && gameTileMap.IsSelectedCharacterIsOnTile(worldPos))
+            {
+                gameTileMap.DeselectCurrentCharacter();
+            }
+            else
+            {
+                gameTileMap.SelectTile(worldPos);
+            }
+        }
+    }
+}
