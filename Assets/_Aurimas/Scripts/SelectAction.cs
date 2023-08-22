67ef56b9a3e3b247476aa1673b4aa756dfe629ed
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2023-08-22 14:34:10 +0300

    Extension button and Select Action

--- Assets/_Aurimas/Scripts/SelectAction.cs
+++ Assets/_Aurimas/Scripts/SelectAction.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SelectAction : MonoBehaviour
 {
+    private ActionManagerNew actionManagerNew;
+    private List<Ability> playerAbilities;
+    private GameObject currentPlayer;
+    [SerializeField] private List<SelectActionButton> abilityButtons;
     void Start()
     {
         
@@ -13,5 +17,10 @@ public class SelectAction : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void GetAbilities()
+    {
+        playerAbilities = currentPlayer.GetComponent<ActionManagerNew>().ReturnAbilities();
     }
 }
---
+++ Assets/_Aurimas/Scripts/SelectActionButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectActionButton : MonoBehaviour
+{
+    private SelectAction selectAction;
+    private int abilityIndex;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+}
