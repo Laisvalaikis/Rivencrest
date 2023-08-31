afdb55f634bc0440e0b1795bea0bf9ef9df02323
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2023-08-31 12:12:19 +0300

    Blaze semi done

---
+++ Assets/_Aurimas/Scripts/Abilities/Blaze.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaze : BaseAction
+{
+    public int bonusDamage = 4;
+    void Start()
+    {
+        actionStateName = "Blaze";
+    }
+
+    public override void ResolveAbility(Vector3 position)
+    {
+        if (CanTileBeClicked(position))
+        {
+            base.ResolveAbility(position);
+            // ChunkData chunkData = GetSpecificGroundTile(position);
+            // bool aflame = chunkData.GetCurrentPlayerInformation().Aflame != null;
+            // if (!aflame)
+            // {
+            //     chunkData.GetCurrentPlayerInformation().Aflame = gameObject;
+            // }
+            // else
+            // {
+            //     TriggerAflame(target);
+            // }
+            //grizti prie aflame veliau
+        }
+    }
+
+    public override void OnTileHover(GameObject tile)
+    {
+        
+    }
+    public override void OffTileHover(GameObject tile)
+    {
+        DisablePreview(tile);
+    }
+
+    public void TriggerAflame(ChunkData centerChunk, int radius)//pakeisti ji i public override void veliau jei kels problemu
+    {
+        if (centerChunk != null && centerChunk.GetCurrentPlayerInformation().Aflame != null &&
+            centerChunk.GetCurrentPlayerInformation().health > 0)
+        {
+            (int centerX, int centerY) = centerChunk.GetIndexes();
+            // GameTileMap.Tilemap.EnableAllTiles();
+
+            for (int i = 1; i <= radius; i++)
+            {
+                List<(int, int)> positions = new List<(int, int)>
+                {
+                    (centerX, centerY + i), // Up
+                    (centerX, centerY - i), // Down
+                    (centerX + i, centerY), // Right
+                    (centerX - i, centerY) // Left
+                };
+            }
+        }
+    }
+}
