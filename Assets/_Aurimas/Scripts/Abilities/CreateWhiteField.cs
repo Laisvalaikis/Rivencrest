33991827bab670c6e9c55373b04a60eac087da49
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2023-09-07 15:12:22 +0300

    CreateWhiteField needs fixing

---
+++ Assets/_Aurimas/Scripts/Abilities/CreateWhiteField.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWhiteField : BaseAction
+{
+    [SerializeField] private GameObject whiteFieldPrefab;
+    
+    public override void ResolveAbility(Vector3 position)
+    {
+        for (int i = 0; i < _chunkList.Count; i++)
+        {
+            spawnedCharacter = Instantiate(whiteFieldPrefab, _chunkList[i].GetPosition() + new Vector3(0.015f, -0.5f, 0), Quaternion.identity);
+        }
+        base.ResolveAbility(position);
+        FinishAbility();
+    }
+    public override void OnTileHover(GameObject tile)
+    {
+        foreach (List<GameObject> MovementTileList in this.AvailableTiles)
+        {
+            EnableTextPreview(tile, MovementTileList, "");
+        }
+    }
+    public override void OffTileHover(GameObject tile)
+    {
+        foreach (List<GameObject> MovementTileList in this.AvailableTiles)
+        {
+            DisablePreview(tile, MovementTileList);
+        }
+    }
+}
