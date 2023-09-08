a80f9ab09da2a3dd9980198bedb023444e8377a8
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2023-09-08 15:21:43 +0300

    CrowAttack

--- Assets/_Aurimas/Scripts/Abilities/CrowAttack.cs
+++ Assets/_Aurimas/Scripts/Abilities/CrowAttack.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
-public class CrowAttack : MonoBehaviour
+public class CrowAttack : BaseAction
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Poison> _poisons;
+    private int poisonBonusDamage=2;
+    public override void ResolveAbility(Vector3 position)
     {
+        for (int i = 0; i < _chunkList.Count; i++)
+        {
+            //ChunkData target = GetSpecificGroundTile(_chunkList[i].GetPosition());
+            if (_chunkList[i].CharacterIsOnTile())
+            {
+                int bonusDamage = 0;
+                if (_poisons.Count > 0)
+                {
+                    bonusDamage += poisonBonusDamage;
+                }
+                DealRandomDamageToTarget(_chunkList[i],minAttackDamage + bonusDamage, maxAttackDamage + bonusDamage);
+            }
+        }
+        base.ResolveAbility(position);
+        FinishAbility();
+    }
+    public override void OnTurnStart()
+    {
+        base.OnTurnStart();
+        PoisonPlayer();
+    }
+    private void PoisonPlayer()
+    {
+        foreach (Poison x in _poisons)
+        {
+            if (x.poisonValue > 0 && x.chunk.GetCurrentPlayerInformation().GetHealth() > 0)
+            {
+                DealDamage(x.chunk, x.poisonValue, false);
+            }
+            x.turnsLeft--;
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void OnTileHover(GameObject tile)
     {
-        
+        EnableDamagePreview(tile, MergedTileList, minAttackDamage, maxAttackDamage);
+    }
+    public override void OffTileHover(GameObject tile)
+    {
+        DisablePreview(tile, MergedTileList);
     }
 }
