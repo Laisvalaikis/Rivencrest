c2f9e2b2680fb56ad42814218b9a0f76f0a5f85a
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2023-09-08 17:17:24 +0300

    CyroFreeze will fix later

---
+++ Assets/_Aurimas/Scripts/Abilities/CyroFreeze.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyroFreeze : BaseAction
+{
+   private PlayerInformation _playerInformation;
+   private bool isAbilityActive = false;
+   
+   public override void OnTurnStart()//pradzioj ejimo
+   {
+      if (isAbilityActive && (GetComponent<PlayerInformation>().health > 0))
+      {
+         StartCoroutine(ExecuteAfterTime(0.2f, () =>
+         {
+            transform.Find("CharacterModel").GetComponent<Animator>().SetBool("stasis", false);
+            GetComponent<PlayerInformation>().Stasis = false;
+         }));
+         //
+         StartCoroutine(ExecuteAfterTime(0.4f, () =>
+         {
+            var pushDirectionVectors = new List<(int, int)>
+            {
+               (1, 0),
+               (0, 1),
+               (-1, 0),
+               (0, -1)
+            };
+            foreach (var x in pushDirectionVectors)
+            {
+               if (CheckIfSpecificLayer(gameObject, x.Item1, x.Item2, groundLayer)) //animation on ground
+               {
+                  GetSpecificGroundTile(gameObject, x.Item1, x.Item2, groundLayer).transform.Find("mapTile").Find("VFXImpactBelow").gameObject.GetComponent<Animator>().SetTrigger("white1");
+               }
+               if (CheckIfSpecificTag(gameObject, x.Item1, x.Item2, blockingLayer, "Player"))
+               {
+                  GameObject target = GetSpecificGroundTile(gameObject, x.Item1, x.Item2, blockingLayer);
+                  if (isAllegianceSame(target))
+                  {
+                     DealRandomDamageToTarget(target, minAttackDamage / 2, maxAttackDamage / 2);
+                  }
+                  else
+                  {
+                     DealRandomDamageToTarget(target, minAttackDamage, maxAttackDamage);
+                  }
+               }
+            }
+         }));
+      }
+   public override void ResolveAbility(Vector3 position)
+   {
+      base.ResolveAbility(position);
+      isAbilityActive = true;
+      _playerInformation.Stasis = true;
+      FinishAbility();
+   }
+}
