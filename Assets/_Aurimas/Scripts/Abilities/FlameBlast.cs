0f3ff2ddea2484734d50447cc056d7c4b88052bf
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2023-09-12 14:22:10 +0300

    FlameBlast A.I only i guess

---
+++ Assets/_Aurimas/Scripts/Abilities/FlameBlast.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameBlast : BaseAction
+{
+    public override void ResolveAbility(Vector3 position)
+    {
+        base.ResolveAbility(position);
+        if (CheckIfSpecificTag(position, 0, 0, blockingLayer, "Player") && (!isAllegianceSame(position) || friendlyFire))
+            {
+                ChunkData chunkData = GetSpecificGroundTile(position);
+                DealRandomDamageToTarget(chunkData, minAttackDamage, maxAttackDamage);
+            }
+        FinishAbility();
+    }
+    public override void OnTileHover(GameObject tile)
+    {
+        EnableDamagePreview(tile, minAttackDamage, maxAttackDamage);
+    }
+    
+    public override void OffTileHover(GameObject tile)
+    {
+        DisablePreview(tile, MergedTileList);
+    }
+}
