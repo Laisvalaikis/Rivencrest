2899fede05e7159a31ec8ffeb74c7549673d8ca6
Patrikas <patrikas.armalis@gmail.com>
2023-09-14 13:52:38 +0300

    Added front slash

---
+++ Assets/_Patrikas/FrontSlash.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FrontSlash : BaseAction
+{
+    private ChunkData[,] _chunkArray;
+    
+    public override void ResolveAbility(Vector3 position)
+    {
+        base.ResolveAbility(position);
+        //tile.transform.Find("mapTile").Find("VFXImpactUpper").gameObject.GetComponent<Animator>().SetTrigger("undead4");
+        //tile.transform.Find("mapTile").Find("VFXImpactBelow").gameObject.GetComponent<Animator>().SetTrigger("undead6");
+        ChunkData chunkData = GameTileMap.Tilemap.GetChunk(position);
+        int index = FindChunkIndex(chunkData);
+        if (index != -1)
+        {
+            for (int i = 0; i < _chunkArray.GetLength(1); i++)
+            {
+                ChunkData damageChunk = _chunkArray[index, i];
+                DealRandomDamageToTarget(damageChunk, minAttackDamage, maxAttackDamage);
+            }
+            //transform.Find("CharacterModel").GetComponent<Animator>().SetTrigger("playerChop");
+            FinishAbility();
+        }
+    }
+    
+    private int FindChunkIndex(ChunkData chunkData)
+    {
+        int index = -1;
+        for (int i = 0; i < _chunkArray.GetLength(1); i++)
+        {
+            if (_chunkArray[0,i] != null)
+            {
+                if (_chunkArray[0,i] == chunkData)
+                {
+                    index = 0;
+                    
+                }
+            }
+            
+            if(_chunkArray[1,i] != null)
+            {
+                if (_chunkArray[1,i] == chunkData)
+                {
+                    index = 1;
+                    
+                }
+            }
+            
+            if (_chunkArray[2,i] != null)
+            {
+                if (_chunkArray[2,i] == chunkData)
+                {
+                    index = 2;
+                    
+                }
+            }
+
+            if (_chunkArray[3,i] != null)
+            {
+                if (_chunkArray[3,i] == chunkData)
+                {
+                    index = 3;
+                    
+                }
+            }
+
+        }
+        return index;
+    }
+
+    private int globalIndex = -1;
+    public override void OnMoveHover(ChunkData hoveredChunk, ChunkData previousChunk)
+    {
+        if (hoveredChunk == previousChunk) return;
+        if (globalIndex != -1)
+        {
+            for (int i = 0; i < _chunkArray.GetLength(1); i++)
+            {
+                ChunkData chunkToHighLight = _chunkArray[globalIndex, i];
+                chunkToHighLight?.GetTileHighlight().SetHighlightColor(Color.green);
+            }
+        }
+        if (hoveredChunk != null && hoveredChunk.GetTileHighlight().isHighlighted)
+        {
+            
+            globalIndex = FindChunkIndex(hoveredChunk);
+            if (globalIndex != -1)
+            {
+                for (int i = 0; i < _chunkArray.GetLength(1); i++)
+                {
+                    ChunkData chunkToHighLight = _chunkArray[globalIndex, i];
+                    chunkToHighLight?.GetTileHighlight().SetHighlightColor(Color.magenta);
+                }
+            }
+        }
+    }
+    
+    public override void CreateGrid(ChunkData centerChunk, int radius)
+    {
+        //Merging into one list
+        (int centerX, int centerY) = centerChunk.GetIndexes();
+        _chunkList.Clear();
+        int count = AttackRange; // -2
+        
+        _chunkArray = new ChunkData[4,count];
+
+        int start = 1;
+        for (int i = 0; i < count; i++) 
+        {
+            if (GameTileMap.Tilemap.CheckBounds(centerX + i + start, centerY))
+            {
+                ChunkData chunkData = GameTileMap.Tilemap.GetChunkDataByIndex(centerX + i + start, centerY);
+                _chunkList.Add(chunkData);
+                HighlightGridTile(chunkData);
+                _chunkArray[0, i] = chunkData;
+            }
+            if (GameTileMap.Tilemap.CheckBounds(centerX - i - start, centerY))
+            {
+                ChunkData chunkData = GameTileMap.Tilemap.GetChunkDataByIndex(centerX-i - start, centerY);
+                _chunkList.Add(chunkData);
+                HighlightGridTile(chunkData);
+                _chunkArray[1, i] = chunkData;
+            }
+            if (GameTileMap.Tilemap.CheckBounds(centerX, centerY + i + start))
+            {
+                ChunkData chunkData = GameTileMap.Tilemap.GetChunkDataByIndex(centerX, centerY + i + start);
+                _chunkList.Add(chunkData);
+                HighlightGridTile(chunkData);
+                _chunkArray[2, i] = chunkData;
+            }
+            if (GameTileMap.Tilemap.CheckBounds(centerX, centerY - i - start))
+            {
+                ChunkData chunkData = GameTileMap.Tilemap.GetChunkDataByIndex(centerX, centerY - i - start);
+                _chunkList.Add(chunkData);
+                HighlightGridTile(chunkData);
+                _chunkArray[3, i] = chunkData;
+            }
+        }
+
+    }
+    
+    public override void CreateGrid()
+    {
+        ChunkData startChunk = GameTileMap.Tilemap.GetChunk(transform.position);
+        CreateGrid(startChunk, AttackRange);
+    }
+    
+    public override void OnTurnEnd()
+    {
+        base.OnTurnEnd();
+    }
+
+    public override void OnTileHover(GameObject tile)
+    {
+        EnableDamagePreview(tile, minAttackDamage, maxAttackDamage);
+    }
+    
+    public override void OffTileHover(GameObject tile)
+    {
+        DisablePreview(tile, MergedTileList);
+    }
+    
+}
